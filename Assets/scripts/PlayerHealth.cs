2cc6b14fb4d87281890fde0054da116ec8d1ab1d
Arthur Brugiere <arbr1@hotmail.fr>
2016-11-21 19:42:04 +0100

    Création de la barre de stress (UI Canvas)

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 0;
+	public float currentHealth;
+	public Slider healthSlider;
+	public float stressAmount = 10;
+
+
+	void Awake () {
+		// Met le nom de la var public dans une var privée
+		currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Remplie la barre de stress
+		currentHealth += stressAmount*Time.deltaTime;
+		healthSlider.value = currentHealth;
+	}
+}
