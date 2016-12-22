d31998656c7e32e805e5ce665c7c665f6dbd0d2a
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-22 18:16:01 +0100

    Life bar (no bonus point)

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,18 +7,23 @@ public class PlayerHealth : MonoBehaviour {
 	public int startingHealth = 0;
 	public float currentHealth;
 	public Slider healthSlider;
-	public float stressAmount = 10;
+	public Image fillBar;
+	public float stressAmount = 1f;
+	public Color beginningColor;
+	public Color gameOverColor;
 
 
 	void Awake () {
 		// Met le nom de la var public dans une var privée
 		currentHealth = startingHealth;
+		stressAmount = stressAmount / 100;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		// Remplie la barre de stress
 		currentHealth += stressAmount*Time.deltaTime;
 		healthSlider.value = currentHealth;
+		fillBar.color = Color.Lerp (beginningColor, gameOverColor, healthSlider.value/ healthSlider.maxValue);
 	}
 }
