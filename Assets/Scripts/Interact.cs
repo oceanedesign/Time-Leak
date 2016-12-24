bb47bfbe23b0f558fd358ff3884a0c157b19a58e
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-24 16:19:48 +0100

    Door interaction [Button E]
    Open that fucking door! \o/

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Interact : MonoBehaviour {
+
+	//Public
+	public enum objectT{ door, ammo, clue, other }
+	public objectT TypeDeLObjet;
+	public float AntiSpam = 0.5f;
+
+	//Dev life <3
+	private float canInteract;
+	//	Door
+	private bool playerInZone, doorOpen = false;
+	private int smooth = 2;
+	private int DoorOpenAngle = 90;
+	private Vector3 defaultRot;
+	private Vector3 openRot;
+
+	// Use this for initialization
+	void Start () {
+		canInteract = AntiSpam;
+		defaultRot = transform.eulerAngles;
+		openRot = new Vector3 (defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		canInteract -= Time.deltaTime;
+
+		switch (TypeDeLObjet) {
+			case objectT.door:
+				if (!doorOpen) {	//	Close door
+					transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+				} else {	// Open door
+					transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
+				}
+
+				if( playerInZone && Input.GetButton("Interact") && canInteract < 0 ){
+					canInteract = AntiSpam;
+					doorOpen = !doorOpen;
+				}
+			break;
+
+			case objectT.ammo:
+				print ("Ce sont des munitions! Piou piou piou!!");
+			break;
+				
+			case objectT.clue:
+				print ("C'est un indice!");
+			break;
+
+			case objectT.other:
+				print ("Arthur, t'as pas pensé à tout!");
+			break;
+
+			default:
+				print ("[Error] Switch mal utilisé");
+			break;
+		}
+
+	}
+
+/*	// Too many conflict -> prefab => too many try
+	void OnGUI(){
+		if (playerInZone) {
+			GameObject.Find ("interactText").GetComponent<Text> ().enabled = true;
+		} else {
+			GameObject.Find ("interactText").GetComponent<Text> ().enabled = false;
+		}
+	}
+*/
+
+	//Activate the Main function when player is near the door
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			playerInZone = true;
+		}
+	}
+
+	//Deactivate the Main function when player is go away from door
+	void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") {
+			playerInZone = false;
+		}
+	}
+}
