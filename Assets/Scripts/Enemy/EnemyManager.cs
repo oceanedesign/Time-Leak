57b1c0e124088d809536b7cd820c580f4d202ab5
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-27 23:49:21 +0100

    Rangement
    Je suis un peu maniaque dans mon chez moi <3

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+	
+	public GameObject[] enemyList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Trigger activation
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			/*
+			for(enemy in enemyList){
+
+				enemy.attack = true;
+
+			}
+			*/
+		}
+	}
+}
