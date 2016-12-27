b6107f676c1a7eae5b75dd39110407be1a3ddcde
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-27 22:36:10 +0100

    AmmoManager

---
+++ Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AmmoManager : MonoBehaviour {
+
+	public static int ammo;
+	Text text;
+
+
+	void Awake ()
+	{
+		text = GetComponent <Text> ();
+		ammo = 0;
+	}
+
+
+	void Update ()
+	{
+		text.text = ""+ammo;
+	}
+}
