5df288373128afa159a3219444d5669a69003468
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-29 21:00:20 +0100

    Destroy ton légo

---
+++ Assets/Scripts/Other/Destructor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructor : MonoBehaviour {
+
+	public int timeToLive = 2;
+
+	// Use this for initialization
+	void Start() {
+		Destroy(gameObject, timeToLive);
+	}
+
+}
