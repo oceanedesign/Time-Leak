4030ae0e0b100c00fc3d749ea747fe5a5999d8c3
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-30 16:49:09 +0100

    THE flashlight
    Press F

---
+++ Assets/Scripts/Player/PlayerCommand.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCommand : MonoBehaviour {
+
+	GameObject torche;
+
+	float canEnableFlashlight, AntiSpam = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+	//	torche = this.gameObject.transform.Find ("flashLight").gameObject;
+		torche = gameObject.transform.GetChild(0).GetChild(1).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		canEnableFlashlight -= Time.deltaTime;
+
+		//Flashlight
+		if ( Input.GetButton ("Flashlight") && canEnableFlashlight < 0) {
+			canEnableFlashlight = AntiSpam;
+
+			if (torche.activeSelf)
+				torche.SetActive (false);
+			else
+				torche.SetActive (true);
+		}
+
+	}
+}
