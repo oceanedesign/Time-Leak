db04b272a6744146788d4f6062f90d9ef67d76be
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-30 16:09:51 +0100

    HUD damage

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -12,11 +12,17 @@ public class PlayerHealth : MonoBehaviour {
 	public Color beginningColor;
 	public Color gameOverColor;
 
+	static bool damaged = false;
+	float flashSpeed = 5f;
+	Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+	Image damageImage;
 
 	void Awake () {
 		// Met le nom de la var public dans une var privée
 		currentHealth = startingHealth;
 		stressAmount = stressAmount / 100;
+
+		damageImage = GameObject.Find ("damageImage").GetComponent<Image> ();
 	}
 
 	// Update is called once per frame
@@ -25,9 +31,21 @@ public class PlayerHealth : MonoBehaviour {
 		currentHealth += stressAmount*Time.deltaTime;
 		healthSlider.value = currentHealth;
 		fillBar.color = Color.Lerp (beginningColor, gameOverColor, healthSlider.value/ healthSlider.maxValue);
+	
+		//HUD dmg
+		if(damaged)
+		{
+			damageImage.color = flashColour;
+		}
+		else
+		{
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
 	}
 
 	public static void takeDmg(int dmg){
 		currentHealth += ((float)dmg / 10);
+		damaged = true;
 	}
 }
