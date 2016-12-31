67ecbb68a8b70c9e3c4b01e2008805d6aa9ccefd
Arthur Brugiere <arbr1@hotmail.fr>
2016-12-31 15:44:26 +0100

    Player death + reload (+ bugfix light) [script only]

---
+++ Assets/Scripts/Other/GameOverManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverManager : MonoBehaviour
+{
+	public float restartDelay = 5f;         // Time to wait before restarting the level
+
+
+	Animator anim;                          // Reference to the animator component.
+	float restartTimer;                     // Timer to count up to restarting the level
+
+
+	void Awake ()
+	{
+		// Set up the reference.
+//		anim = GetComponent <Animator> ();
+	}
+
+
+	void Update ()
+	{
+		// If the player has run out of health...
+		if(PlayerHealth.player_isDead){
+			print ("You'll restart");
+			// ... tell the animator the game is over.
+	//		anim.SetTrigger ("GameOver");
+
+			// .. increment a timer to count up to restarting.
+			restartTimer += Time.deltaTime;
+
+			// .. if it reaches the restart delay...
+			if(restartTimer >= restartDelay)
+			{
+				// .. then reload the currently loaded level.
+				UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,6 +2,9 @@
 using UnityEngine.UI;
 using System.Collections;
 
+// For disabled controller script
+using UnityStandardAssets.Characters.FirstPerson;
+
 public class PlayerHealth : MonoBehaviour {
 
 	public int startingHealth = 0;
@@ -12,7 +15,7 @@ public class PlayerHealth : MonoBehaviour {
 	public Color beginningColor;
 	public Color gameOverColor;
 
-	static bool damaged = false;
+	public static bool player_damaged, player_isDead = false;
 	float flashSpeed = 5f;
 	Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 	Image damageImage;
@@ -33,7 +36,7 @@ public class PlayerHealth : MonoBehaviour {
 		fillBar.color = Color.Lerp (beginningColor, gameOverColor, healthSlider.value/ healthSlider.maxValue);
 	
 		//HUD dmg
-		if(damaged)
+		if(player_damaged)
 		{
 			damageImage.color = flashColour;
 		}
@@ -41,11 +44,29 @@ public class PlayerHealth : MonoBehaviour {
 		{
 			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
 		}
-		damaged = false;
+		player_damaged = false;
+
+		//Die
+		if (healthSlider.value == healthSlider.maxValue && !player_isDead)
+			Death ();
 	}
 
 	public static void takeDmg(int dmg){
 		currentHealth += ((float)dmg / 10);
-		damaged = true;
+		player_damaged = true;
 	}
+
+	void Death ()
+	{
+		player_isDead = true;
+
+		print ("You die");
+
+//		playerAudio.clip = deathClip;
+//		playerAudio.Play ();
+
+		GetComponent<FirstPersonController>().enabled = false;
+		//playerShooting.enabled = false;
+	}
+
 }
