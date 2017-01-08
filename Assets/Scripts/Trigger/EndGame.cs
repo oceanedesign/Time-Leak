6dd56c9cdeab3cb1a480bee29b772cce29bf64ab
Unknown <oceanemariel@hotmail.fr>
2017-01-08 10:39:25 +0100

    Trigger

---
+++ Assets/Scripts/Trigger/EndGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour {
+
+	private GameObject player;
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject == player)
+		{
+			SceneManager.LoadScene ((SceneManager.GetActiveScene().buildIndex)+1); // utiliser le nom de la scéne de fin de jeu en mode victoire.
+		}
+
+	}
+}
