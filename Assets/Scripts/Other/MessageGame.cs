32883282bc48c0f67a833123f71ac2bfc66c5a23
Unknown <oceanemariel@hotmail.fr>
2017-01-15 16:45:00 +0100

    petit message tuto

---
+++ Assets/Scripts/Other/MessageGame.cs
@@ -0,0 +1,17 @@
+﻿﻿using UnityEngine; 
+using System.Collections; 
+using UnityEngine.UI; 
+
+public class MessageGame : MonoBehaviour { 
+
+	public string message; 
+
+	// Use this for initialization 
+	void Start () { 
+
+	} 
+
+	void OnDestroy(){ 
+		GameObject.Find ("lockedText").GetComponent<Text> ().text = message; 
+	} 
+} 
