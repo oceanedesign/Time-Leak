2abed2e73aedc5d78f6f05dcb3b5d09163222ec8
Arthur Brugiere <arbr1@hotmail.fr>
2017-01-15 16:57:50 +0100

    MessageGame update -> interaction
    fais attention à la booléenne !

--- Assets/Scripts/Other/MessageGame.cs
+++ Assets/Scripts/Other/MessageGame.cs
@@ -1,15 +1,28 @@
-﻿﻿using UnityEngine; 
+﻿using UnityEngine; 
 using System.Collections; 
 using UnityEngine.UI; 
 
 public class MessageGame : MonoBehaviour { 
 
 	public string message; 
+	public bool messageOnInteract = false;
+	Interact interactInstance;
 
 	// Use this for initialization 
 	void Start () { 
-
+		if (messageOnInteract) {
+			interactInstance = GetComponent<Interact> ();
+		}
 	} 
+
+	void Update(){
+		if (messageOnInteract) {
+			if ( interactInstance.isInteract() ) {
+				GameObject.Find ("lockedText").GetComponent<Text> ().text = message; 
+				Destroy (this);
+			}
+		}
+	}
 
 	void OnDestroy(){ 
 		GameObject.Find ("lockedText").GetComponent<Text> ().text = message; 
