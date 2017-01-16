12eca44c4677d9ba09495ee06a0e14271c806ed0
Unknown <oceanemariel@hotmail.fr>
2017-01-16 14:11:38 +0100

    ajout texte tuto

---
+++ Assets/Scripts/Other/LoadSceneGameOver.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneGameOver : MonoBehaviour {
+
+	public void LoadSceneG(){
+		SceneManager.LoadScene ((SceneManager.GetActiveScene().buildIndex));
+	} 
+
+}
