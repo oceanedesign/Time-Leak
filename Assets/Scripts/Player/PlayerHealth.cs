95eb446444fb5fb885f6740a9dc4ea2764c50a90
Arthur Brugiere <arbr1@hotmail.fr>
2017-01-16 21:06:11 +0100

    OldJS Delete + Player scripts commentés

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,6 +7,7 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerHealth : MonoBehaviour {
 
+	// Variable publiques (dans l'inspecteur)
 	public int startingHealth = 0;
 	public Slider healthSlider;
 	public Image fillBar;
@@ -18,6 +19,7 @@ public class PlayerHealth : MonoBehaviour {
 	public static bool player_damaged, player_heal, player_isDead = false;
 	public static bool stressMode;
 
+	// Varible privées
 	float flashSpeed = 5f;
 	Color flashColourDmg = new Color(1f, 0f, 0f, 0.1f);
 	Color flashColourHeal = new Color(0f, 0f, 1f, 0.1f);
@@ -26,11 +28,12 @@ public class PlayerHealth : MonoBehaviour {
 	void Awake () {
 		// Met le nom de la var public dans une var privée
 		currentHealth = startingHealth;
-		stressAmount = stressAmount / 100;
+		stressAmount = stressAmount / 100;	// Adapte la vitesse à la barre
 
-		damageImage = GameObject.Find ("damageImage").GetComponent<Image> ();
+		damageImage = GameObject.Find ("damageImage").GetComponent<Image> ();	// Stock l'Image dans la variable
 		player_isDead = false;
 
+		// N'active la progression du stress en début de niveau que au niveau 2
 		if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "niveau2")
 			stressMode = true;
 		else
@@ -39,7 +42,7 @@ public class PlayerHealth : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//Debug
+		//Debug	=>	La valeur minimal ne peut pas être inférieur à celle du slider
 		if (currentHealth < healthSlider.minValue) {
 			currentHealth = healthSlider.minValue;
 		}
@@ -50,7 +53,7 @@ public class PlayerHealth : MonoBehaviour {
 		}
 		//Life update value
 		healthSlider.value = currentHealth;
-		fillBar.color = Color.Lerp (beginningColor, gameOverColor, healthSlider.value/ healthSlider.maxValue);
+		fillBar.color = Color.Lerp (beginningColor, gameOverColor, healthSlider.value/ healthSlider.maxValue);	// Couleur de la barre de stress
 
 
 		//HUD dmg/heal
@@ -66,18 +69,20 @@ public class PlayerHealth : MonoBehaviour {
 				damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
 			}
 		}
-		player_damaged = player_heal = false;
+		player_damaged = player_heal = false;	// Desactive les deux booleans
 
 		//Die
 		if (healthSlider.value == healthSlider.maxValue && !player_isDead)
 			Death ();
 	}
 
+	// Fonction statique publique pour faire subir des degats depuis d'autres scripts
 	public static void takeDmg(int dmg){
 		currentHealth += ((float)dmg / 10);
 		player_damaged = true;
 	}
 
+	// Fonction statique publique pour faire soigner depuis d'autres scripts
 	public static void takeHeal(int heal){
 		currentHealth -= ((float)heal / 10);
 		player_heal = true;
@@ -86,14 +91,8 @@ public class PlayerHealth : MonoBehaviour {
 	void Death ()
 	{
 		player_isDead = true;
-
-		print ("You die");
-
-//		playerAudio.clip = deathClip;
-//		playerAudio.Play ();
-
+		// Desactive les mouvements du joueur
 		GetComponent<FirstPersonController>().enabled = false;
-		//playerShooting.enabled = false;
 	}
 
 }
--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -11,17 +11,22 @@ public class PlayerWeapon : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Recupere le nombre de prefab pour l'optimisation de la boucle dans le Update
 		listAmmoSize = ammoPrefab.Length;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		//Lancer de bordel
-		if( Input.GetButtonDown("Fire1") && AmmoManager.ammo > 0 && canvasPause.activeSelf==false){
-			GameObject lego = (GameObject)Instantiate(ammoPrefab[ Random.Range(0, listAmmoSize) ], new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+		
+		if( Input.GetButtonDown("Fire1") && AmmoManager.ammo > 0 && canvasPause.activeSelf==false){	// S'il y a des munitions et que le joueur tire
+			GameObject lego = (GameObject)Instantiate(	// Instancie un objet a lancer
+									ammoPrefab[ Random.Range(0, listAmmoSize) ], // Choisis un prefab aleatoirement parmis l'array de prefab
+									new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation	// Place l'objet dans l'espace de la scene
+							  );
+			// Donne une impulsion au prefab a projeters
 			lego.GetComponent<Rigidbody> ().AddForce (-transform.forward * thrust, ForceMode.Impulse);
 
+			// Appel la fonction "shot" du script "AmmoManager"
 			AmmoManager.shot ();
 		}
 
