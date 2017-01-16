254a0c1f33536cd497a83013f4f9e11aefdb3165
Arthur Brugiere <arbr1@hotmail.fr>
2017-01-16 15:04:06 +0100

    Flamethrower Boss dmg

---
+++ Assets/Scripts/Enemy/Boss/BossFlamethrower.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossFlamethrower : MonoBehaviour {
+
+	public float attackSpeed = 1f;
+	public int degat = 1;
+
+	float canAttack;
+	bool playerInZone = false;
+
+	// Use this for initialization
+	void Start () {
+		canAttack = attackSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		canAttack -= Time.deltaTime;
+
+		//Attack
+		if (playerInZone && canAttack < 0) {
+			canAttack = attackSpeed;
+			PlayerHealth.takeDmg (degat);
+		}
+
+	}
+
+	//Activate the Main function when player is near the door
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			playerInZone = true;
+		}
+	}
+
+	//Deactivate the Main function when player is go away from door
+	void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") {
+			playerInZone = false;
+		}
+	}
+}
